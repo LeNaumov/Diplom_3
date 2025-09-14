28fe04dd79570fda40013e05576892ea83e19a16
Aleksey Naumov <SurprezeBoy@ya.ru>
2025-09-14 13:45:18 +0300

    Создал папку с файлами tests

---
+++ tests/conftest.py
@@ -0,0 +1,42 @@
+import pytest
+from helpers.api import ApiMethods
+from helpers.data import Ingredient
+from helpers.utils import register_new_user_and_return_login_password, register_new_user_and_order
+from selenium import webdriver
+
+
+# Фабрика для создания WebDriver на основе переданного имени браузера
+class WebdriverFactory:
+
+    @staticmethod
+    def getWebdriver(browserName):
+        if browserName == "firefox":
+            return webdriver.Firefox()
+        elif browserName == "chrome":
+            return webdriver.Chrome()
+        else:
+            raise ValueError(f"Unsupported browser: {browserName}")
+
+# Функция добавляет аргумент командной строки '--browser' для выбора браузера
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="chrome", help="Browser to run tests on")
+
+@pytest.fixture
+def browser(request):
+    browser_name = request.config.getoption("--browser")  # Получаем значение из --browser
+    driver = WebdriverFactory.getWebdriver(browser_name)  # Создаем WebDriver
+    yield driver  # Передаем WebDriver в тест
+    driver.quit()
+
+@pytest.fixture
+def register():
+    register = register_new_user_and_return_login_password()
+    yield register
+    ApiMethods.delete_user(register[0], register[1], register[2])
+
+@pytest.fixture
+def order():
+    order = register_new_user_and_order()
+    ApiMethods.create_order(order[0], order[1], Ingredient.list_of_ingredients)
+    yield order
+    ApiMethods.create_user(order[0], order[1], order[2])
